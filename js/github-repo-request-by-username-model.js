d7dfa06b4344ba9436523faaf7a155c62d7d37fa
Kevin VanEvery <kvanever@gmail.com>
2016-08-05 11:47:31 -0700

    refactor for loop into forEach method

--- js/github-repo-request-by-username-model.js
+++ js/github-repo-request-by-username-model.js
@@ -4,9 +4,7 @@ PublicRepositories = function(){};
 
 PublicRepositories.prototype.repoRequest = function(username, publicReposNameAndDescription) {
   $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
-    for(i = 0; i < response.length; i++){
-      publicReposNameAndDescription(response[i]);
-    }
+    response.forEach(publicReposNameAndDescription);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
